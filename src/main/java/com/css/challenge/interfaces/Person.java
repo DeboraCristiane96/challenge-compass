061c73dd4f7654b934f61f81a4bc42326b2583ea
Edilson Santos <edilson0982021@gmail.com>
2023-07-26 16:36:35 -0300

    Adding attributes to Person.class

--- src/main/java/com/css/challenge/interfaces/Person.java
+++ src/main/java/com/css/challenge/interfaces/Person.java
@@ -1,4 +1,18 @@
 package com.css.challenge.interfaces;
 
 abstract class Person {
+
+    private String name;
+    private String email;
+    private String emailCompass;
+    private String cpf;
+    private boolean status;
+    private String birthDay;
+    private String maritalStatus;
+    private Character sex;
+    private String phone;
+    private String adress;
+    private int zipCode;
+    private String naturalNess;
+
 }
