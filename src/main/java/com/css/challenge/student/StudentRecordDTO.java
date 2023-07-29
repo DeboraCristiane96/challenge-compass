573509d30f6bacd720454353c89b8dc85468277a
Edilson Santos <edilson0982021@gmail.com>
2023-07-29 18:34:49 -0300

    Completed Student table relationships

--- src/main/java/com/css/challenge/student/StudentRecordDTO.java
+++ src/main/java/com/css/challenge/student/StudentRecordDTO.java
@@ -1,4 +1,6 @@
 package com.css.challenge.student;
 
-public record StudentRecordDTO(int period,String course,String college,String name,String email,String emailCompass,String cpf,boolean status,String birthDay,String civilStatus,Character sex,String phone,String adress,int zipCode,String naturalNess) {
+import com.css.challenge.Class.ClassEntity;
+
+public record StudentRecordDTO(int period, String course, String college, String name, String email, String emailCompass, String cpf, boolean status, String birthDay, String civilStatus, Character sex, String phone, String adress, int zipCode, String naturalNess, ClassEntity classEntity) {
 }
